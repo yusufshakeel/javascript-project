93af81fae1c1817bacb38758969fc16345a78f1c
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-05-19 11:45:29 +0530

    update

---
+++ js/output.js
@@ -0,0 +1,21 @@
+// alert("Hello World");
+
+// alert('Hello World');
+
+// alert(1 + 2);
+
+// alert("This is our output: " + 10);
+
+// document.write("Hello World");
+
+// document.write('Hello World');
+
+// document.write(5 - 4);
+
+// document.write("Result: " + (10 + 20));
+
+// console.log("Hello World");
+
+// console.log(7 * 5);
+
+console.log("Result: " + (20 - 10));
