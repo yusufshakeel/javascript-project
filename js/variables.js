7473706fc9602aeeb420e3165b1043726f2f22c3
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-05-22 16:05:37 +0530

    update

---
+++ js/variables.js
@@ -0,0 +1,30 @@
+/**
+ * in javascript we create a variable using the var keyword
+ *
+ * we can use the following characters to name a variable
+ * 
+ * letters	a-z A-Z
+ * digits   0-9
+ * dollar   $
+ * underscore _
+ *
+ * note! variable can't start with a digit
+ */
+
+var username = "Yusuf Shakeel";
+
+console.log(username);
+
+var firstname, lastname, points;
+
+console.log(firstname);
+console.log(lastname);
+console.log(points);
+
+var username;
+
+console.log(username);
+
+username = "Some New Value";
+
+console.log(username);
