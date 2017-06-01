f56a7f38bb82375cdf6dca127276d640cd8acf7a
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-06-01 15:32:41 +0530

    update

---
+++ js/assignment-operator.js
@@ -0,0 +1,65 @@
+/**
+ * assignment operator =
+ */
+/*
+var x = 10;
+console.log(x);
+
+var y = 20;
+console.log(y);
+y = y + 5;
+console.log(y);
+
+var a = 5;
+console.log(a);
+a = a - 2;
+console.log(a);
+*/
+
+/**
+ * shorthand notation
+ * combining assignment operator and arithmetic operators
+ */
+
+//addition
+/*
+var k = 10;
+console.log(k);
+//k = k + 5;
+k += 5;
+console.log(k);
+*/
+
+//subtraction
+/*
+var k = 10;
+console.log(k);
+//k = k - 5;
+k -= 5;
+console.log(k);
+*/
+
+//multiplication
+/*
+var k = 5;
+console.log(k);
+//k = k * 5;
+k *= 5;
+console.log(k);
+*/
+
+//division
+/*
+var k = 5;
+console.log(k);
+//k = k / 2;
+k /= 2;
+console.log(k);
+*/
+
+//modulus
+var k = 5;
+console.log(k);
+//k = k % 2;
+k %= 2;
+console.log(k);
---
+++ js/comparison-operators.js
@@ -0,0 +1,61 @@
+/**
+ * comparison operators
+ */
+
+// is equal to
+/*
+var x = 10;
+var y = 11;
+console.log(x == y);
+*/
+
+// not equal to
+/*
+var x = 10;
+var y = 10;
+console.log(x != y);
+*/
+
+// is equal to and of same type
+/*
+var x = 10;
+var y = "10";
+console.log(typeof x);
+console.log(typeof y);
+console.log(x === y);
+*/
+
+// is not equal to or not of same type
+/*
+var x = 10;
+var y = "10";
+console.log(typeof x);
+console.log(typeof y);
+console.log(x !== y);
+*/
+
+// greater than
+/*
+var x = 10;
+var y = 15;
+console.log(x > y);
+*/
+
+// less than
+/*
+var x = 200;
+var y = 100;
+console.log(x < y);
+*/
+
+// greater than or equal to
+/*
+var x = 9;
+var y = 10;
+console.log(x >= y);
+*/
+
+// less than or equal to
+var x = 11;
+var y = 10;
+console.log(x <= y);
