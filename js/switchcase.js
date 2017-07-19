1daf57098a3cebfc16e017ba0603a871ae3e2425
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-07-19 15:04:30 +0530

    update

---
+++ js/switchcase.js
@@ -0,0 +1,92 @@
+/*
+switch ( value ) {
+
+	case val1:
+		//some code goes here...
+		break;
+
+	case val2:
+		//some code goes here...
+		break;
+
+	default:
+		//some code goes here...
+
+}
+*/
+
+/*
+var x = 100;
+
+switch (x) {
+
+	case 1:
+		console.log("x is 1");
+		break;
+
+	case 2:
+		console.log("x is 2");
+		break;
+
+	case 3:
+		console.log("x is 3");
+		break;
+
+	//default:
+	//	console.log("x is something else");
+
+}
+
+
+console.log("end of code");
+*/
+
+/*
+var x = 10;
+
+switch (x) {
+
+	case 1:
+	case 2:
+	case 3:
+		console.log("x is 1 or 2 or 3");
+		break;
+
+	case 4:
+	case 5:
+	case 6:
+		console.log("x is 4 or 5 or 6");
+		break;
+
+	default:
+		console.log("x is something else");
+}
+
+console.log("end of code");
+*/
+
+var x = 'qwerty';
+
+switch (x) {
+
+	case 'Hello':
+		console.log("Hello");
+		break;
+
+	case 'Hi':
+		console.log("Hi");
+		break;
+
+	default:
+		console.log("something else");
+
+}
+
+console.log("end of code");
+
+
+
+
+
+
+
