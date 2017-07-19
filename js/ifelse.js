772a5e6d81875970036ca36ac158b6745908dbde
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-07-19 14:03:05 +0530

    update

---
+++ js/ifelse.js
@@ -0,0 +1,46 @@
+/*
+var x = 200;
+
+if ( x > 100 ) {
+
+	console.log(x);
+
+}
+*/
+
+/*
+var x = 0;
+
+if ( x > 0 ) {
+	console.log("x is greater than 0");
+}
+
+console.log("end of code");
+*/
+
+/*
+var x = -1;
+
+if (x > 0) {
+	console.log("value of x greater than 0");
+}
+else {
+	console.log("value of x not greater than 0");
+}
+
+console.log("end of code");
+*/
+
+var x = 0;
+
+if (x > 0) {
+	console.log("value of x greater than 0");
+}
+else if (x < 0) {
+	console.log("value of x less than 0");
+}
+else {
+	console.log("value of x is 0");
+}
+
+console.log("end of code");
