aedae25e7c66751eeb906ef60f88d2c8b6518fde
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-07-24 15:25:37 +0530

    update

---
+++ js/whileloop.js
@@ -0,0 +1,56 @@
+/*
+document.write("<p>Hello World</p>");
+document.write("<p>Hello World</p>");
+document.write("<p>Hello World</p>");
+document.write("<p>Hello World</p>");
+document.write("<p>Hello World</p>");
+document.write("<p>Hello World</p>");
+document.write("<p>Hello World</p>");
+document.write("<p>Hello World</p>");
+document.write("<p>Hello World</p>");
+document.write("<p>Hello World</p>");
+*/
+
+/*
+var c = 1;
+
+// c = 1 and c <= 10 is true
+// c = 2 and c <= 10 is true
+// c = 3 and c <= 10 is true
+// c = 4 and c <= 10 is true
+// c = 5 and c <= 10 is true
+// c = 6 and c <= 10 is true
+// c = 7 and c <= 10 is true
+// c = 8 and c <= 10 is true
+// c = 9 and c <= 10 is true
+// c = 10 and c <= 10 is true
+// c = 11 and c <= 10 is false
+while ( c <= 10 ) {
+	document.write("<p>" + c + " Hello World</p>");
+
+	// c = 11
+	c = c + 1;
+}
+*/
+
+
+var c = 1;
+
+// c = 1
+// c = 2
+// c = 3
+do {
+	document.write("<p>" + c + " Hello World</p>");
+
+	// c = 4
+	c = c + 1;
+} while ( c <= 3 );
+// c = 2 and c <= 3 is true
+// c = 3 and c <= 3 is true
+// c = 4 and c <= 3 is false
+
+
+
+
+
+
